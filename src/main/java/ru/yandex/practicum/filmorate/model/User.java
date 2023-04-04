8dc2c55f39ac97bb9df39eef18b5aedbccd0102d
Vlas <dron.2001sv@mail.ru>
2023-04-04 19:50:43 +0300

    <refactor><Добавить новый Set для лайков и методы для работы с ним>

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +16,13 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+    private Set<Integer> friendList = new HashSet<>();
 
+    public void addFriend(int id) {
+        friendList.add(id);
+    }
+
+    public void deleteFriend(int id) {
+        friendList.remove(id);
+    }
 }
